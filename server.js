c5def4ed9a7a38b04af6e9af81507ea929b2e5ea
Andrew Bianco <andrewbianco98@gmail.com>
2021-06-11 18:02:49 -0400

    Finsihed the basic html files, set up the server, and connected all of the webpages

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('<h1> Welcome to Hot Restaurant! </h1>');
+
+const tables = [
+    {
+        routeName: 'exampleTable',
+        name: 'John Test',
+        phone: 1236547890,
+        email: 'jtest@example.com',
+        tableId: 2,
+    },
+];
+
+const waitlist = [
+    {
+        routeName: 'exampleWaitList',
+        name: 'John Test',
+        phone: 1236547890,
+        email: 'jtest@example.com',
+        tableId: 3,
+    },
+];
+
+// Routes
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+// Displays all Tables and Waitlist
+app.get('/api/tables', (req, res) => res.json(tables));
+
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
+
+app.post('/api/resere', (req, res) => {
+    const newReservation = req.body;
+    
+    newReservation.routeName = newReservation.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newReservation);
+    
+    tables.push(newReservation);
+    res.json(newReservation);
 });
 
 app.listen(PORT, () => {
